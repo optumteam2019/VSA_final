82b5a56c281f4a442bb4b0a3963035003e917ae0
Mathur <rmathur5@ms.ds.uhc.com>
2019-07-11 14:58:49 +0530

    Changes full name

--- index.js
+++ index.js
@@ -42,10 +42,8 @@ app.post('/dialogflow', express.json(), (req, res) => {
 
   function name_fun (agent)
   {
-      var person_name = agent.parameters["given-name"]
-      var person_last_name = agent.parameters["last-name"];
+      var person_name = agent.parameters["given-name"] + ' ' + agent.parameters["last-name"];
       pr_name = person_name;
-      pr_last_name = person_last_name;
       //console.log('Name : ' + person_name);
       const person_age = agent.parameters["age"];
       //console.log(person_age)
