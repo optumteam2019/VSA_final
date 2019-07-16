279d8207be544e506ec3f592aabe663993c09850
Mathur <rmathur5@ms.ds.uhc.com>
2019-07-16 12:44:55 +0530

    Changes Final

--- index.js
+++ index.js
@@ -37,10 +37,7 @@ app.get('/update', function(req, res){
 app.post('/update2', function(res, req){
   var result = JSON.stringify(req.body);
   
-  if(pr_gender=="" && result.Gender!="")
-  {
-    pr_gender = result.Gender;
-  }
+  res.send(result);
 })
 
 app.post('/', function(req, res){
