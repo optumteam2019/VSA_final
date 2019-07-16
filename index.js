72334d82db31aa8b36f0a9d049b8034bfaab5984
Mathur <rmathur5@ms.ds.uhc.com>
2019-07-16 14:58:42 +0530

    Changes Final

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ app.post('/dialogflow', express.json(), (req, res) => {
       agent.add('Please provide your current zipcode to continue.')
     }
 
-    else if(pr_plan==null)
+    else if(pr_plan=="")
     {
       agent.add('Select a plan from the following available list of plans : \nPART C\nPART D')
     }
