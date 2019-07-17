47ad713853cda917129b39b17decb4b891f85628
Mathur <rmathur5@ms.ds.uhc.com>
2019-07-17 14:25:33 +0530

    Changes Final

--- index.js
+++ index.js
@@ -26,6 +26,8 @@ app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'))
 
 app.get('/update', function(req, res){
   var result={"Name":pr_name, "Age":pr_age, "Zipcode":pr_zipcode, "Gender":pr_gender,"medc_num":pr_medicare, "datea":pr_datea.slice(0,10), "dateb":pr_dateb.slice(0,10), "Plan":pr_plan, "Enroll":pr_enroll}; 
+  pr_enroll = ""
+  //console.log(result);
   res.send(result);
 });
 
